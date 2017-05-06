4c9c3bd326d7063792ae088336ac4cd829d93e1a
root <zhouquan0x16@gmail.com>
2017-05-06 20:47:05 +0000

    add db client using pool

--- server/server.js
+++ server/server.js
@@ -1,30 +1,42 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import proxy from 'http-proxy'
 import path from 'path'
+import pool from './db.js'
+
 const app = express()
+const apiProxy = proxy.createProxyServer()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
 const router = express.Router()
 
-const staticFiles = express.static(path.join(__dirname, '../../client/build'))
-app.use(staticFiles)
-
-router.get('/cities', (req, res) => {
+// api for history records
+router.get('/api/history/nodes', (req, res) => {
   console.log(`reading this from env > ${process.env.MY_VARIABLE}`)
-  const cities = [
-    {name: 'New York City', population: 8175133},
-    {name: 'Los Angeles',   population: 3792621},
-    {name: 'Chicago',       population: 2695598}
-  ]
-  res.json(cities)
+  pool.connect().then(client => {
+    client.query('select time, gw_rssi,applicationid,gw_snr,tx_adr,battery_voltage from packet;').then(result => {
+      client.release()
+      console.log('successfully query', result)
+      res.json(result)
+    })
+    .catch(e => {
+      client.release()
+      console.error('query error', e.message, e.stack)
+      res.status(500).send('Something broke in db!')
+    })
+  })
+
+})
+
+// redirect to golang server
+router.all('*', (req, res) => {
+  console.log(`redirect to golang server`)
+  swaggerProxy.web(req, res, {target: "https://localhost:8080", secure: false});
 })
 
 app.use(router)
-
-// any routes not picked up by the server api will be handled by the react router
-app.use('/*', staticFiles)
 
 app.set('port', (process.env.PORT || 8899))
 app.listen(app.get('port'), () => {
