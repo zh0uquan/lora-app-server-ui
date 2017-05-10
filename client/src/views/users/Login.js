e12d3ea4990ac3a6d775eb4895808c0f928471d8
QuanZhou <zhouquan0x16@gmail.com>
2017-05-10 19:26:53 +0200

    minor fix

--- client/src/views/users/Login.js
+++ client/src/views/users/Login.js
@@ -29,7 +29,7 @@ class Login extends Component {
   }
 
   onSubmit(e) {
-    e.preventDefault(); 
+    e.preventDefault();
     SessionStore.login(this.state.login, (token) => {
       this.context.router.push("/");
     });
@@ -37,25 +37,29 @@ class Login extends Component {
 
   render() {
     return(
-      <div>
-        <ol className="breadcrumb">
-          <li className="active">Login</li>
-        </ol>
-        <hr />
-        <div className="panel panel-default">
-          <div className="panel-body">
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <label className="control-label" htmlFor="username">Username</label>
-                <input className="form-control" id="username" type="text" placeholder="username" required value={this.state.login.username || ''} onChange={this.onChange.bind(this, 'username')} />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-6 col-sm-offset-3 col-xs-12">
+            <ol className="breadcrumb">
+              <li className="active">Login</li>
+            </ol>
+            <hr />
+            <div className="panel panel-default">
+              <div className="panel-body">
+                <form onSubmit={this.onSubmit}>
+                  <div className="form-group">
+                    <label className="control-label" htmlFor="username">Username</label>
+                    <input className="form-control" id="username" type="text" placeholder="username" required value={this.state.login.username || ''} onChange={this.onChange.bind(this, 'username')} />
+                  </div>
+                  <div className="form-group">
+                    <label className="control-label" htmlFor="password">Password</label>
+                    <input className="form-control" id="password" type="password" placeholder="password" value={this.state.login.password || ''} onChange={this.onChange.bind(this, 'password')} />
+                  </div>
+                  <hr />
+                  <button type="submit" className="btn btn-primary pull-right">Login</button>
+                </form>
               </div>
-              <div className="form-group">
-                <label className="control-label" htmlFor="password">Password</label>
-                <input className="form-control" id="password" type="password" placeholder="password" value={this.state.login.password || ''} onChange={this.onChange.bind(this, 'password')} />
-              </div>
-              <hr />
-              <button type="submit" className="btn btn-primary pull-right">Login</button>
-            </form>
+            </div>
           </div>
         </div>
       </div>
